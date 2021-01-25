8a53ac6942a2a232e9238b88ee50e64e7dcedbbf
ZeynepKar06 <77546727+ZeynepKar06@users.noreply.github.com>
2021-01-25 16:09:30 +0300

    Add files via upload

---
+++ GerçekMusteri.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    class GerçekMusteri:Musteri
+    {
+        public string TcNo { get; set; }
+        public string MusteriAdi { get; set; }
+        public string MusteriSoyadi { get; set; }
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GerçekMusteri musteri1 = new GerçekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.MusteriAdi = "ZEYNEP";
+            musteri1.MusteriSoyadi="KARAKUŞ";
+            musteri1.TcNo = "12345678912";
+
+
+            //Kodlama.oi
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "Kodlama.oi";
+            musteri2.VergiNo = "123456789";
+
+
+            //iki nesne var gerçek müşteri/ tüzel müşreti
+
+
+            Musteri musteri3 = new GerçekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Ekle(musteri3);
+            customerManager.Ekle(musteri4);
+
+
+        }
+    }
+}
